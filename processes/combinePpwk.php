1bf707bb7ae7ff7ba26faae27d29ac98306c7eed
outlawstar4761 <outlawstar4761@gmail.com>
2018-06-25 12:52:30 -0500

    Update combinePpwk.php

--- processes/combinePpwk.php
+++ processes/combinePpwk.php
@@ -4,13 +4,15 @@
 class CombinePpwk{
 
     protected $gbl_dps;
+    protected $pythonFile;
 
     public function __construct($gbl_dps){
         $this->gbl_dps = $gbl_dps;
+        $this->pythonFile = __DIR__ . '/python/oaPaperWork.py';
         $this->_combinePpwk();
     }
     protected function _combinePpwk(){
-        $output = shell_exec("python python/oaPaperWork.py " . escapeshellarg($this->gbl_dps));
+        $output = shell_exec("python " . $this->pythonFile . " " . escapeshellarg($this->gbl_dps));
         if($output){
             die(print_r(error_get_last()));
         }
